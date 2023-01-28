f33ab0940577ff00c90603b3846a2eaf72fd43b7
Serge Kripkovsky <122611492+Custodiant@users.noreply.github.com>
2023-01-29 00:16:09 +0700

    Добавлено решение для 4 задачи в отрицательном диапазоне

--- HomeWork001/Task4/Program.cs
+++ HomeWork001/Task4/Program.cs
@@ -5,21 +5,48 @@
 
 Console.Clear();
 
-Console.WriteLine("Давайте посмотрим все положительные четные числа, от 1 до необходимого вам!");
+Console.WriteLine("Давайте посмотрим все четные числа, от 0 до необходимого вам!");
 Console.WriteLine("***");
 
 Console.Write("Введите число: ");
 int num = int.Parse(Console.ReadLine());
 int count = 2;
+int count2 = -2;
 Console.WriteLine("---");
 
 if (num < count)
 {
-    Console.WriteLine("Ваше число вне диапазона!");
+    if (num > count2)
+    {
+        Console.WriteLine("В диапазоне от 0 до вашего числа - нет четных чисел!");
+    }
+    else
+    {
+        Console.WriteLine("Все четные числа - отрицательные.");
+        Console.WriteLine("---");
+        Console.Write("Числа в диапазоне: ");
+        while (num < count2)
+        {
+            Console.Write(count2 + ", ");
+            count2 --;
+            count2 --;
+        }
+        if (count2 == num)
+        {
+            Console.WriteLine(num);
+        }
+        else
+        {
+            Console.WriteLine();
+        }
+    }    
 }
+
 else
 {
-    Console.Write("Все четные чила в диапазоне: ");
+    Console.WriteLine("Все четные числа - положительные."); 
+    Console.WriteLine("---");
+    Console.Write("Числа в диапазоне: ");
     while (num > count)
     {
     Console.Write(count + ", ");
@@ -34,10 +61,6 @@ else
     {                           //Эти строки нужны, для визуального оформления и не более!!
         Console.WriteLine();    //Эти строки нужны, для визуального оформления и не более!!
     }                           //Эти строки нужны, для визуального оформления и не более!!
-    if (num < count)
-    {
-        Console.WriteLine("Ваше число вне диапазона!");
-    }
 }
 Console.WriteLine("***");
 
