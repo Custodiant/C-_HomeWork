d4b037f5219fa490b6a9715f0879208f767800fa
Serge Kripkovsky <122611492+Custodiant@users.noreply.github.com>
2023-01-31 02:43:46 +0700

    добавлено решение первой задачи

---
+++ HomeWork002/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.WriteLine("Введите трёхзначное число и программа покажет вам вторую цифру!");
+Console.WriteLine("***");
+Console.WriteLine("Ваше число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("---");
+
+int a = num / 100;
+int b = num % 10;
+int c = (num - a*100 - b) / 10;
+
+//Console.WriteLine($"Первая цифра вашего числа - это {a}");
+Console.WriteLine($"Вторая цифра вашего числа - это {c}");
+//Console.WriteLine($"Вторая цифра вашего числа - это {b}");
+
+Console.WriteLine("***");
+Console.WriteLine("Программа, выполнена");
