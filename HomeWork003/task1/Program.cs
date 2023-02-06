c5259a9b08df97fbe549a1f20a525c01829c6f56
Serge Kripkovsky <122611492+Custodiant@users.noreply.github.com>
2023-02-07 00:14:55 +0700

    создано 3 задачи, начато решение первого задания

---
+++ HomeWork003/task1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.Clear();
+
+Console.WriteLine("Является ли ваше число палиндромом?");
+Console.WriteLine("***");
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int num_rev = 0;
+int b = 0;
+
+while (num > 0)
+{
+    b = num % 10;
+    num = num / 10;
+    num_rev = b + (num_rev * 10);
+}
+
+Console.WriteLine("--");
+Console.WriteLine(num_rev);
+Console.WriteLine("--");
+
+if (num_rev == num)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("Ваше число - ПАЛИНДРОМ!");
+    Console.ForegroundColor = ConsoleColor.White;
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Ваше число - НЕ палиндром!");
+    Console.ForegroundColor = ConsoleColor.White;    
+}
+Console.WriteLine("***");
+Console.WriteLine("Программа, выполнена!");
