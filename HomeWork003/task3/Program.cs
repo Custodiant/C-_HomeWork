7d49de0cf251d2d68007e4a4d07bcf74852a284a
Serge Kripkovsky <122611492+Custodiant@users.noreply.github.com>
2023-02-07 22:37:35 +0700

    добавлено решение 3 задачи

--- HomeWork003/task3/Program.cs
+++ HomeWork003/task3/Program.cs
@@ -3,3 +3,30 @@
 
 //3 -> 1, 8, 27
 //5 -> 1, 8, 27, 64, 125
+
+
+Console.Clear();
+Console.WriteLine("Давайте построим таблицу кубов всех чисел, от 1 до любого вашего числа.");
+Console.WriteLine("***");
+Console.Write("Введите целое число: ");
+int num = int.Parse(Console.ReadLine()!)!;
+Console.WriteLine("---");
+
+int count = 1;
+int qCount = 1;
+while (count < num)
+{
+    qCount = count*count*count;
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.Write($"{qCount}, ");
+    Console.ForegroundColor = ConsoleColor.White;
+    count++;
+}
+qCount = count*count*count;
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.Write($"{qCount}. ");
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine();
+Console.WriteLine("***");
+Console.WriteLine("Программа, выполнена!");
