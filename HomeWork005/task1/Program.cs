4f6a069f847f11d1c7034bf362f8bff5b8f814b8
CustodiantSK <kripkovsky@yandex.ru>
2023-02-15 00:31:50 +0700

    Добавлено решение первой задачи

--- HomeWork005/task1/Program.cs
+++ HomeWork005/task1/Program.cs
@@ -4,3 +4,61 @@
 
 //[345, 897, 568, 234] -> 2
 
+Console.Clear();
+int[] array = new int[10];
+
+void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(100, 1000);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    for (int position = 0; position < count; position++)
+    {
+        Console.Write($"{col[position]} ");
+    }
+}
+
+void EvenArray(int[] collect)
+{
+    int count = collect.Length;
+    int SumCountEven = 0;
+    for (int i = 0; i < count; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            SumCountEven++;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write($"{array[i]} ");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+    if (SumCountEven == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Чётных чисел нет!");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    else
+    {
+        Console.WriteLine("- чётные числа");
+        Console.Write("Итого, чётных чисел: ");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(SumCountEven);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
+
+FillArray(array);
+Console.Write("Наш рандомный массив: ");
+PrintArray(array);
+Console.WriteLine();
+EvenArray(array);
