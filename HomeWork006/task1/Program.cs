10112499aa14a5ffbe33bc54069f786290daca21
CustodiantSK <kripkovsky@yandex.ru>
2023-02-19 00:33:01 +0700

    Добавлено решение первой задачи

---
+++ HomeWork006/task1/Program.cs
@@ -0,0 +1,51 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 */ //В примере некорректно согласно описанию. Нама требуются положительные числа, а не более 1
+
+void UserNumbers(double[] collection)
+{
+    int lenght = collection.Length;
+    for (int index = 0; index < lenght; index++)
+    {
+        collection[index] = double.Parse(Console.ReadLine()!);
+    }
+}
+
+void PrintUserNumbers(double[] collection2)
+{
+    int count = collection2.Length;
+    for (int position = 0; position < count; position++)
+    {
+        Console.Write($"{collection2[position]} ");
+    }
+}
+
+void EvenArray(double[] collection3)
+{
+    double counter = collection3.Length;
+    double evenSum = 0;
+    for (int even = 0; even < counter; even++)
+    {
+        if (collection3[even] > 0)
+        {
+            evenSum++;
+        }
+    }
+    Console.Write($"{evenSum}.");
+}
+
+Console.Clear();
+
+double[] array = new double[5];
+
+Console.WriteLine($"Введите любые числа: ");
+UserNumbers(array);
+Console.WriteLine($"Ваши числа: ");
+PrintUserNumbers(array);
+Console.WriteLine();
+Console.WriteLine($"Количество положительных: ");
+EvenArray(array);
+Console.WriteLine();
